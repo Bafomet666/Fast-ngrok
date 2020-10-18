2adfa65bf6e2dd3803dc84e421aa55f13b63912b
Maxx <72593411+Bafomet666@users.noreply.github.com>
2020-10-18 18:06:38 +0000

    Add files via upload

---
+++ termux-ngrok.sh
@@ -0,0 +1,64 @@
+#!/bin/dash
+# bafomet
+atualizar='\033[1;32m'
+echo "$atualizar Atualizando repositório..."
+apt update && upgrade -y
+apt install -y wget
+clear
+k='\033[01;32m'
+echo
+echo "${k} ____         __                     _    ";
+echo "${k}|  _ \       / _|                   | |   ";
+echo "${k}| |_) | __ _| |_ ___  _ __ ___   ___| |_  ";
+echo "${k}|  _ < / _` |   / _ \|  _ ` _ \ / _ \ __| ";
+echo "${k}| |_) | (_| | || (_) | | | | | |  __/ |_  ";
+echo "${k}|____/ \__,_|_| \___/|_| |_| |_|\___|\__| ";
+
+echo
+echo " Вы хотите установить Ngrok? [Y/n]"
+read opcao
+case $opcao in
+y)
+echo
+echo " Скачивание Termux-ngrok ..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo " Неизвестная архитектура "
+esac
+
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+chmod 700 /data/data/com.termux/files/usr/bin/ngrok
+rm ngrok ngrok.zip
+clear
+echo "${k}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
+echo "${k}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
+echo "${k}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
+echo "${k}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
+echo "${k}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
+echo "${k}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
+echo
+echo "Пример команды | ngrok http 8080)"
+;;
+
+n)
+clear
+echo "Ngrok не установлен :("
+echo
+esac
+
+
+
